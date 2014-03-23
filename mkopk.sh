730ea02ed87f7b63250f4462f7baf6ccf9965626
jxv <joevargas@hush.com>
2014-03-23 13:37:20 -0700

    opk

---
+++ mkopk.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+rm -r build skane.opk
+make -f Makefile.gcw0
+make -f Makefile
+mkdir build
+mkdir build/res
+cp skane build
+cp icon.png build
+cp default.gcw0.desktop build
+cp -r res/* build/res
+mksquashfs build skane.opk -all-root -noappend -no-exports -no-xattrs
